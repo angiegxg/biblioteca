5544a67e96d9cc27874b751f3a485fcb2bd9bc63
angiegxg <angiegg7258@gmail.com>
2023-08-04 11:55:08 +0200

    Se modifica home

--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import BooksList from '../../BooksList/BooksList'
 import { useDispatch, useSelector } from 'react-redux'
 import { getBooks } from '../../redux/actions'
-import { addToRead } from '../../redux/actions'
+
 
 function Home() {
 const books= useSelector(state=>state.disponibilityBooks)
